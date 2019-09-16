199624b7baa566e91fceca24017fd6e0e875a40f
Bogdan Doroschenko <bogdandoroschenko@gmail.com>
2019-09-16 08:00:23 +0300

    initial todoList is empty

--- src/App.js
+++ src/App.js
@@ -4,21 +4,11 @@ import './App.css';
 import TodoList from './TodoList';
 import TodoInput from './TodoInput';
 
-const initialTodos = [
-  {id: 6, title: 'user can check todo'},
-  {id: 5, title: 'user can update todo'},
-  {id: 4, title: 'user can delete todo'},
-  {id: 3, title: 'user can create todo'},
-  {id: 2, title: 'refactoring. Delete dead code'},
-  {id: 1, title: 'stylization list'},
-  {id: 0, title: 'display todo list'},
-];
-
 class App extends React.Component {
   constructor() {
     super();
     this.state = { 
-      todos: initialTodos,
+      todos: [],
       lastId: 6
     }
 
