f06d86f4c6dc18080a138ec24a7785915aad1f28
Bogdan Doroschenko <bogdandoroschenko@gmail.com>
2019-09-30 16:53:25 +0300

    Updates

---
+++ precache-manifest.f60ed41f3ec641ec4d03cf17e9281c72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf309406ee8056ef9f6a759502a054d4",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "48a8521fc3c24d4c98f9",
+    "url": "/todoapp/static/css/main.79296c46.chunk.css"
+  },
+  {
+    "revision": "f9688a43331c9bc4fb84",
+    "url": "/todoapp/static/js/2.199cdf2d.chunk.js"
+  },
+  {
+    "revision": "48a8521fc3c24d4c98f9",
+    "url": "/todoapp/static/js/main.5fd3f24a.chunk.js"
+  },
+  {
+    "revision": "2ab533bab6fb99934a05",
+    "url": "/todoapp/static/js/runtime~main.6f2569c4.js"
+  }
+]);
